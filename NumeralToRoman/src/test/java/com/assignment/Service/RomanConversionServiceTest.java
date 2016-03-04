ee4dc4291ecd22625763a2a61589dd675b88b798
Manasa Rangaswamy <manasa.rangaswamy@Aconite-Tech.local>
2016-03-04 11:25:15 +0000

    Added more test cases

--- NumeralToRoman/src/test/java/com/assignment/Service/RomanConversionServiceTest.java
+++ NumeralToRoman/src/test/java/com/assignment/Service/RomanConversionServiceTest.java
@@ -12,13 +12,27 @@ public class RomanConversionServiceTest {
     private RomanConversionService romanConversionService;
 
     @Before
-    public void setup(){
+    public void setup() {
         romanConversionService = new RomanConversionServiceImpl();
     }
 
+
     @Test
-    public void convertToRomanTest(){
+    public void convertToRomanTest_minValue_I(){
         String result = romanConversionService.convertToRoman(1);
-        Assert.assertEquals(result,"I");
+        Assert.assertEquals(result, "I");
     }
+
+    @Test
+    public void convertToRomanTest_maxValue_MMMCMXCIX(){
+        String result = romanConversionService.convertToRoman(3999);
+        Assert.assertEquals("MMMCMXCIX",result);
+    }
+
+    @Test
+    public void convertToRomanTest_RandomValue_MMMCMXCIX(){
+        String result = romanConversionService.convertToRoman(12);
+        Assert.assertEquals("XII",result);
+    }
+
 }
